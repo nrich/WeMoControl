536573b035878d922157fbac947a313f6b97f5bc
Neil Richardson <nrich@ii.net>
2016-04-20 18:58:57 +0800

    add controls for new functions

--- control.pl
+++ control.pl
@@ -6,14 +6,11 @@ use warnings;
 use Getopt::Std qw/getopts/;
 use Data::Dumper qw/Dumper/;
 
-use Net::UPnP::ControlPoint qw//;
-use XML::LibXML qw//;
-
 use lib qw/lib/;
 use Wemo::Bridge qw//;
 
 my %opts = ();
-getopts('o:d:', \%opts);
+getopts('o:d:f:t:', \%opts);
 main(@ARGV);
 
 sub main {
@@ -25,21 +22,31 @@ sub main {
 
     for my $light (@{$bridge->lights()}) {
         if ($light->FriendlyName() eq $name) {
-            $opts{o} ? $light->on() : $light->off();
-
             my $dim = $opts{d};
+            $light->dim($dim, $opts{t}) if defined $dim;
 
-            $light->dim($dim) if defined $dim;
+            if (defined $opts{o}) {
+                $opts{o} ? $light->on() : $light->off();
+            }
+
+            if (defined $opts{f}) {
+                $opts{f} < 0 ? $light->fadeout(-$opts{f}) : $light->fadein($opts{f});
+            }
         }
     }
 
     for my $group (@{$bridge->groups()}) {
         if ($group->GroupName() eq $name) {
-            $opts{o} ? $group->on() : $group->off();
-
             my $dim = $opts{d};
+            $group->dim($dim, $opts{t}) if defined $dim;
 
-            $group->dim($dim) if defined $dim;
+            if (defined $opts{f}) {
+                $opts{f} < 0 ? $group->fadeout(-$opts{f}) : $group->fadein($opts{f});
+            }
+
+            if (defined $opts{o}) {
+                $opts{o} ? $group->on() : $group->off();
+            }
         }
     }
 }
